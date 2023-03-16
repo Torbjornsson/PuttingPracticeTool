6f8d6ce2eaed1b550028f5352a3de038c61ef038
Torbjornsson <david.torbjornsson@gmail.com>
2023-03-16 21:22:03 +0100

    feat(frontend): Add test view

--- PuttingPracticeToolFrontend/src/App.vue
+++ PuttingPracticeToolFrontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RoundStart />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import RoundStart from "@/components/RoundStart.vue";
 
 export default {
   name: 'App',
   components: {
+    RoundStart,
     HelloWorld
   }
 }
---
+++ PuttingPracticeToolFrontend/src/components/RoundStart.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    Test
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "RoundStart"
+}
+</script>
+
+<style scoped>
+
+</style>
